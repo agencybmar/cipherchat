fdb69c0e03d8a8fa06a949e6f11a9ff65245e7b7
agencybmar <agencybmar@gmail.com>
2026-08-22 21:07:40 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
 // 1) Create a Firebase web app in https://console.firebase.google.com/
-// 2) Enable Authentication > Email/Password and Realtime Database.
+// 2) Enable Authentication > Google and Realtime Database.
 // 3) Paste the Firebase web-app config below. This config is designed to be public.
 window.CIPHERCHAT_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyAX-r-v3bIIoFj2Gd8CPTG46YiWHwT_P9k",
+  authDomain: "cipherchat-84dcb.firebaseapp.com",
+  databaseURL: "https://cipherchat-84dcb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cipherchat-84dcb",
+  storageBucket: "cipherchat-84dcb.firebasestorage.app",
+  messagingSenderId: "794901638481",
+  appId: "1:794901638481:web:07d9246859c12eb6f6b770",
+  measurementId: "G-WJWTP90N9Y"
 };
